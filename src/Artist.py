1b85eff04293f2cd7e8db76dce1d059396553d7a
jiafi <jordanwolinsky@gmail.com>
2019-05-26 11:51:15 -0400

    got BFS to work, still need to work on the limits

--- src/Artist.py
+++ src/Artist.py
@@ -15,23 +15,29 @@ class Artist:
     self.__graph = nx.Graph()
     self.__graph.add_node(name)
 
-  def create_graph_dfs(self, limit=5):
-    artist_queue = queue.Queue(maxsize=limit)
+  # BFS with additional processing to create edges and other info
+  # BFS will show more closely related artists than DFS
+  def create_graph_bfs(self, limit):
+    artist_queue = queue.Queue()
     artist_queue.put(self.name)
     visited = set()
-    while(len(self.__artists) <= limit and not artist_queue.empty):
+    while(len(self.__artists) <= limit and not artist_queue.empty()):
       current_artist = artist_queue.get()
       visited.add(current_artist)
       current_features = set()
-      artist = genius.search_artist(current_artist, max_songs=20)
+      artist = genius.search_artist(current_artist, max_songs=2, sort='popularity')
       # Get artists associated with current artist
       for song in artist.songs:
-        current_features.update(song.featured_artists)
-        current_features.update(song.producers)
+        for feature in song.featured_artists:
+          current_features.add(feature['name'])
+        for feature in song.producers:
+          current_features.add(feature['name'])
+
       # Create tuples to add edges
       edges = []
       for feature in current_features:
         # Adding edges and to the queue
+        print(feature)
         if feature not in visited:
           artist_queue.put(feature)
         edges.append((current_artist, feature))
@@ -45,6 +51,6 @@ class Artist:
     plt.show()
 
 genius = lyricsgenius.Genius("Ac918_2foSjtvsZXSydkug3UIGI3cUerNAVVkdhPDsZKM5gbOBkuiRmrXRjtGhmD")
-kanye = Artist("kanye", genius)
-kanye.create_graph_dfs(limit=5)
+kanye = Artist("kanye west", genius)
+kanye.create_graph_bfs(5)
 kanye.show_graph()
