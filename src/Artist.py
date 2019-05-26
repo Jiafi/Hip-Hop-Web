8d592f29b510da847e520e0f67c5043c6d2a8415
jiafi <jordanwolinsky@gmail.com>
2019-05-26 01:38:50 -0400

    creating graph, trynna debug search now

--- src/Artist.py
+++ src/Artist.py
@@ -3,7 +3,7 @@
 import lyricsgenius
 import networkx as nx
 import matplotlib.pyplot as plt
-
+import queue
 class Artist:
 
   def __init__(self, name, genius):
@@ -15,12 +15,12 @@ class Artist:
     self.__graph = nx.Graph()
     self.__graph.add_node(name)
 
-  def  create_graph_dfs(self, limit):
-    queue = Queue(maxsize=limit)
-    queue.put(self.name)
+  def create_graph_dfs(self, limit=5):
+    artist_queue = queue.Queue(maxsize=limit)
+    artist_queue.put(self.name)
     visited = set()
-    while(len(self.__artists) <= limit and not queue.empty):
-      current_artist = queue.get()
+    while(len(self.__artists) <= limit and not artist_queue.empty):
+      current_artist = artist_queue.get()
       visited.add(current_artist)
       current_features = set()
       artist = genius.search_artist(current_artist, max_songs=20)
@@ -33,12 +33,18 @@ class Artist:
       for feature in current_features:
         # Adding edges and to the queue
         if feature not in visited:
-          queue.put(feature)
+          artist_queue.put(feature)
         edges.append((current_artist, feature))
       # add features to graph
       self.__graph.add_edges_from(edges)
       # Done processing features
 
+  def show_graph(self):
+    plt.subplot(121)
+    nx.draw(self.__graph, with_labels=True)
+    plt.show()
 
 genius = lyricsgenius.Genius("Ac918_2foSjtvsZXSydkug3UIGI3cUerNAVVkdhPDsZKM5gbOBkuiRmrXRjtGhmD")
 kanye = Artist("kanye", genius)
+kanye.create_graph_dfs(limit=5)
+kanye.show_graph()
